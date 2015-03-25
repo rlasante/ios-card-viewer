6c0602c19798835dbb18cedb60b892b8fb76c5f2
LaSante, Ryan <ryan.lasante@gmail.com>
2015-03-25 18:29:41 -0400

    Initial commit of iOS project

---
+++ CardViewer/CardViewer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CardViewer
+//
+//  Created by Ryan LaSante on 3/25/15.
+//  Copyright (c) 2015 LaSante. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CardViewer/CardViewer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CardViewer
+//
+//  Created by Ryan LaSante on 3/25/15.
+//  Copyright (c) 2015 LaSante. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
